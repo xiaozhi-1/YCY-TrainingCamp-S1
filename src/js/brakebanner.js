15331a5d3837a0563857d3ef662c39e47c8369c1
xiaozhi-1 <xiaozhifeng_1@foxmail.com>
2022-08-05 11:57:34 +0800

    按钮添加

--- src/js/brakebanner.js
+++ src/js/brakebanner.js
@@ -1,5 +1,35 @@
 class BrakeBanner{
 	constructor(selector){
-		
+		this.app = new PIXI.Application({
+			width: window.innerWidth,
+			height: window.innerHeight,
+			backgroundColor: 0xff0000
+		})
+
+		document.querySelector(selector).appendChild(this.app.view)
+
+		this.loadResources()
+	}
+
+	loadResources () {
+		this.loader = new PIXI.Loader()
+
+		this.loader.add("btn.png", "images/btn.png")
+		this.loader.add("btn_circle.png", "images/btn_circle.png")
+		// this.loader.add("btn.png", "images/btn.png")
+
+		this.loader.load()
+
+		this.loader.onComplete.add(() => {
+			console.log('加载成功')
+			this.renderResource()
+		})
+	}
+
+	renderResource () {
+		let btn = new PIXI.Sprite(this.loader.resources['btn.png'].texture)
+		let btn_circle = new PIXI.Sprite(this.loader.resources['btn_circle.png'].texture)
+		this.app.stage.addChild(btn)
+		this.app.stage.addChild(btn_circle)
 	}
 }
